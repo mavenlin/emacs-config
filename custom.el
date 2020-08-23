1b08bdfc80b04901142cf96150cc8de42677c709
linmin <mavenlin@gmail.com>
2020-08-23 12:40:11 -0400

    change color

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auto-dim-other-buffers-face ((t (:background "black" :foreground "black"))))
+ '(auto-dim-other-buffers-face ((t (:background "color-232" :foreground "black"))))
  '(elfeed-search-title-face ((t (:foreground "color-244"))))
  '(elfeed-search-unread-count-face ((t (:foreground "brightred"))))
  '(elfeed-search-unread-title-face ((t (:foreground "brightwhite")))))
